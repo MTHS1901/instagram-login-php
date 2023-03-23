6cc35cbd264e3530e59e313703bc59c65f7e6cff
MTHS1901 <50026803+MTHS1901@users.noreply.github.com>
2023-03-23 18:55:26 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,77 @@
+<?php
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+// User agent, se não, não vai pai
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36";
+
+$url = "https://www.instagram.com/";
+
+// Request feito em CURL, para tentar obter o csrf_token da página
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+curl_setopt($ch, CURLOPT_REFERER, "https://www.instagram.com/accounts/login/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie_".$username.".txt");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie_".$username.".txt");
+$data = curl_exec($ch);
+curl_close($ch);
+
+$b  = ""; // token
+$cookie_file = "cookie_".$username.".txt";
+$cookie_data = file_get_contents($cookie_file);
+preg_match("/instagram.com\s+TRUE\s+\/\s+TRUE\s+\d+\s+csrftoken\s+([^\s]+)/", $cookie_data, $matches);
+if (isset($matches[1])) {
+    $csrf_token = $matches[1];
+    $b = $csrf_token;
+} else {
+     die("Falha ao obter token");
+}
+
+// echo 'csrftoken: ' . $b . "\n";
+
+// Request para fazer login
+
+$g = round(microtime(true) * 1000); // time aleatorio
+$body = "enc_password=#PWD_INSTAGRAM_BROWSER:0:{$g}:{$password}&username={$username}"; // 0 significa que a senha não esta codificada
+$url = "https://www.instagram.com/api/v1/web/accounts/login/ajax/"; // pra onde vai o request
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "accept: *",
+    "content-type: application/x-www-form-urlencoded",
+    "x-csrftoken: ${b}",
+    "x-ig-www-claim: 0",
+    "x-ig-www-claim: 0",
+    "x-instagram-ajax: 1007167139",
+    "x-requested-with: XMLHttpRequest"
+));
+
+curl_setopt($ch, CURLOPT_REFERER, "https://www.instagram.com/accounts/login/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie_".$username.".txt");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie_".$username.".txt");
+$data = curl_exec($ch);
+curl_close($ch);
+
+echo $data;
+
+//$json = json_decode($data, true);
+//$checkpointUrl = $json['checkpoint_url'];
+//echo $checkpointUrl;
+//file_get_contents($checkpointUrl);
+
+?>
